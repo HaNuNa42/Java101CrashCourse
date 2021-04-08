1768c0e7d6911eb35e517f73727d20e5f9c3e9de
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2021-04-08 14:45:18 +0300

    What are the variables

---
+++ 2_variables/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //case sensitive
+        //Camel Casing
+        //reusebility : kodları tekrar kullanılabilirlik
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci sayısı : ";
+
+        System.out.println("öğrenci sayım : 9");
+        System.out.println("öğrenci sayım : " + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+}
