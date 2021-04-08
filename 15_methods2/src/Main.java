d9259069d1ae4f7a9120f37cb75b6fffc6a32c8e
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2021-04-08 14:56:52 +0300

    methods 2

---
+++ 15_methods2/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String mesaj = "bugün hava çok güzel";
+        String yeniMesaj = mesaj.substring(0, 2);
+        String yeniMesaj2 = sehirVer();
+        System.out.println(yeniMesaj);
+        int sayi = topla(5, 7);
+        System.out.println(sayi);
+        int toplam = topla2(2, 3, 4, 5, 6, 10);
+        System.out.println(toplam);
+    }
+
+    public static void ekle() {
+        System.out.println("eklendi");
+    }
+
+    public static void sil() {
+        System.out.println("silindi");
+    }
+
+    public static void guncelle() {
+        System.out.println("guncellendi");
+    }
+
+    public static int topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    public static int topla2(int... sayilar) {
+        //noktalar bir evi dizi olduğunu belirtir. birden fazla parametre olacağını belirtir.
+        int toplam = 0;
+        for (int sayi : sayilar) {
+            toplam += sayi;
+        }
+        return toplam;
+    }
+
+    public static String sehirVer() {
+        return "ankara";
+    }
+}
