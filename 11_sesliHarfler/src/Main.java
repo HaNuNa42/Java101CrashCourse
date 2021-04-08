384e4effb97263306c16a25d8857d88f924b9dae
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2021-04-08 14:53:28 +0300

    vowels example

---
+++ 11_sesliHarfler/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char harf = 'E';
+
+        switch (harf) {
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println();
+                break;
+            default:
+                System.out.println("İnce sesli harf");
+        }
+    }
+}
