64d8a9297f65244c3f8050cb03bc08f057b123a9
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2021-04-08 14:45:52 +0300

    datatypes

---
+++ 3_datatypes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    //tip güvenli bir dildir
+        int sayi = 12;
+        sayi =13;
+        double sayi2 = 13.0;
+
+        // ascii koduna göre tanımlandığı için char da bir nevi int.tir
+        char karakter = 'A';
+        String metin = "hatice";
+        boolean dogruMu = false;
+    }
+}
