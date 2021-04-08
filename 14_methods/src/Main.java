16d131cfa6fcf666ba4430322e37417d49e96191
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2021-04-08 14:55:43 +0300

    methods

---
+++ 14_methods/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        mesajVer();
+        parametreliFonksiyon(5);
+    }
+
+    public static void mesajVer() {
+        System.out.println("mesaj");
+    }
+    public static void parametreliFonksiyon(int parametre) {
+
+        System.out.println("mesaj" + parametre);
+    }
+}
